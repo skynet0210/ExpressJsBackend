b538e7d3ca4d93f4bfa848cb125c17c2e0edee00
skynet0210 <mauro.02.10.1994@gmail.com>
2024-01-31 23:44:25 -0500

    subida de proyecto

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+
+
+const { logErrors, errorHandler, boomerrorHandler } = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const whitelist = ['http:localhost:5500', 'https://myapp.com', 'http://127.0.0.1:5500'];
+const options = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('no permitido'));
+        }
+    }
+};
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+    res.send('Hola server express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+    res.send('Hola soy una nueva ruta');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomerrorHandler);
+app.use(errorHandler);
+
+
+app.listen(port, () => {
+    console.log('Mi puerto' + port);
+});
---
+++ middlewares/error.handler.js
@@ -0,0 +1,25 @@
+function logErrors(err, req, res, next) {
+    console.log('logErrors');
+    console.error(err);
+    next(err);
+}
+
+function errorHandler(err, req, res, next) {
+    console.log('errorandler');
+    res.status(500).json({
+        message: error.message,
+        stack: err.stack,
+    });
+}
+
+function boomerrorHandler(err, req, res, next) {
+    if (err.isBoom) {
+        const { output } = err;
+        res.status(output.statusCode).json(output.payload);
+    } else {
+        next(err);
+    }
+
+}
+
+module.exports = { logErrors, errorHandler, boomerrorHandler }
